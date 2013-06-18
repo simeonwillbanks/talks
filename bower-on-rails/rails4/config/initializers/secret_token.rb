d3ec3d2a16e4cc739e499141cc3975d82562910e
Simeon F. Willbanks <sfw@simeonfosterwillbanks.com>
2013-06-18 09:29:51 -0700

    rails _4.0.0.rc2_ new rails4

---
+++ bower-on-rails/rails4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4::Application.config.secret_key_base = '89092e77be267d03c8857b2ae8027af8554f6231c15940c3f6b501528e55d838e1b62cd748a4ca3a9ad3a0903ba338cfe8c7e258e7e98ccd3d11a41d40b8a00d'
